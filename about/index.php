f6ad1b0ecbaf52e67838b7c065ba37ca30128513
Roman Kulichkov <roman@kulichkov.pro>
2016-06-19 02:15:08 +0600

    Страница "о нас"

---
+++ about/index.php
@@ -0,0 +1,48 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Информация о компании");
+$APPLICATION->SetTitle("О нас");
+?>
+<img src="/images/about.jpg" width="240" height="158" />
+<p>ООО &quot;УК Дивногорский&quot; оказывает услуги по&nbsp;управлению и&nbsp;обслуживанию жилых домов и&nbsp;коммерческой недвижимости с&nbsp;2003 года. На&nbsp;данный момент в&nbsp;управлении организации находится 257 жилых домов, а&nbsp;это более 25000 квартир.</p>
+<p>Главной целью нашей компании является профессиональное предоставление качественных коммунальных услуг жителям.</p>
+<p>Более 300 высококвалифицированных специалистов ООО &quot;УК Дивногорский&quot; ежедневно стараются обеспечить максимально комфортное проживание для граждан.</p>
+<p>В&nbsp;целях снижения количества претензий, жалоб со&nbsp;стороны населения и&nbsp;повышения качества оказываемых услуг, была создана специальная служба по&nbsp;контролю качества услуг.</p>
+<p>Основными мероприятиями, проводимыми нашей компанией, являются: осуществление общестроительных, инженерных, электромонтажных, специализированных строительных, изоляционных, штукатурных, малярных, <nobr>санитарно-технических</nobr> работ.</p>
+<p>ООО &quot;УК Дивногорский&quot; активно внедряет энергосберегающие технологии, которые позволяют проводить измерения и&nbsp;сбор информации удаленно. Больше половины домов оснащены приборами учета, оборудованными телеметрией.</p>
+<p>Наша управляющая компания обладает обширной <nobr>материально-технической</nobr> базой, имеет личную <nobr>аварийно-ремонтную</nobr> службу, в&nbsp;наличии склады необходимых материалов и&nbsp;запасных частей для предотвращения любой аварийной ситуации.</p>
+<p>Политика ООО &quot;УК Дивногорский&quot; нацелена на&nbsp;чёткое планирование своей деятельности. Успешная деятельность компании показала жизнеспособность и&nbsp;эффективность частного содержания жилья.</p>
+<div class="certificate">
+	<div class="certif-row">
+		<div class="certif-col">
+			<a href="/images/sertifikat-big.jpg" class="certif-fancy">
+				<img src="/images/sertifikat.jpg" alt="" class="certif-img">
+			</a>
+			<h4 class="certif-text">Сертификат УК ЖФ</h4>
+		</div>
+		<div class="certif-col">
+			<a href="/images/sertifikat-big.jpg" class="certif-fancy">
+				<img src="/images/sertifikat.jpg" alt="" class="certif-img">
+			</a>
+			<h4 class="certif-text">Сертификат УК ЖФ</h4>
+		</div>
+		<div class="certif-col">
+			<a href="/images/sertifikat-big.jpg" class="certif-fancy">
+				<img src="/images/sertifikat.jpg" alt="" class="certif-img">
+			</a>
+			<h4 class="certif-text">Сертификат УК ЖФ</h4>
+		</div>
+		<div class="certif-col">
+			<a href="/images/sertifikat-big.jpg" class="certif-fancy">
+				<img src="/images/sertifikat.jpg" alt="" class="certif-img">
+			</a>
+			<h4 class="certif-text">Сертификат УК ЖФ</h4>
+		</div>
+		<div class="certif-col">
+			<a href="/images/sertifikat-big.jpg" class="certif-fancy">
+				<img src="/images/sertifikat.jpg" alt="" class="certif-img">
+			</a>
+			<h4 class="certif-text">Сертификат УК ЖФ</h4>
+		</div>
+	</div>		
+</div> <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
