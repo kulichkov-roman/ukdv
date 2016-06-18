b11b4e010e9c587b6d986f4107e0080aa198345b
Roman Kulichkov <roman@kulichkov.pro>
2016-06-19 02:14:30 +0600

    Включаемая область с информацией о режиме работы

--- include/schedule.php
+++ include/schedule.php
@@ -1,18 +1,22 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<div class="block-title"><a href="javascript:void(0)">Режим работы</a></div>
-<table class="contacts-table">
-    <tbody>
-        <tr>
-            <td class="caption">Пн - Сб:</td>
-            <td>8:00 - 20:00</td>
-        </tr>
-        <tr>
-            <td class="caption">Вск:</td>
-            <td>выходной</td>
-        </tr>
-        <tr>
-            <td class="caption">Обед:</td>
-            <td>12:00 - 13:00</td>
-        </tr>
-    </tbody>
-</table>
+<div class="block-title"><a href="javascript:void(0)">График работы</a></div>
+<div id="schedule-block-container">
+    <div class="org-contacts">
+        <table class="contacts-table">
+            <tbody>
+            <tr>
+                <td class="caption">Пн&nbsp;-&nbsp;Сб:</td>
+                <td>8:00 - 20:00</td>
+            </tr>
+            <tr>
+                <td class="caption">Вск:</td>
+                <td>выходной</td>
+            </tr>
+            <tr>
+                <td class="caption">Обед:</td>
+                <td>12:00 - 13:00</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
